38c6aeab4c69fca5e96b720510c2aec6aea45ea8
zccope <zcope@talltimbers.org>
2022-06-02 11:41:48 -0400

    mod loop for finding fuel height

--- ttrs_quicfire/print_inp_files.py
+++ ttrs_quicfire/print_inp_files.py
@@ -300,21 +300,9 @@ def print_QU_simparams_inp(dom, wind, qf_arrs):
         input_file.write("5 !Number of uniform surface cells\n")
         input_file.write("!dz array (meters)\n")
         fuel_height = 0
-        found = True
         for k in range(len(qf_arrs.rhof)):
             if np.max(qf_arrs.rhof[k]) != 0:
                 fuel_height = k+1
-            # if not found:
-            #     break
-            # found = False
-            # for i in range(len(qf_arrs.rhof[0][0])):
-            #     for j in range(len(qf_arrs.rhof[0])):
-            #         if qf_arrs.rhof[k][j][i] != 0:
-            #             fuel_height = k
-            #             found = True
-            #             break
-            #     if found:
-            #         break
         relief = 0
         if qf_arrs.use_topo:
             relief = qf_arrs.topo.max() - qf_arrs.topo.min()
